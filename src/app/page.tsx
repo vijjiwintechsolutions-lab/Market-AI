0f8335670743f79c73aaeae6e07050026f68aac7
vijjiwintechsolutions-lab <vijjiwintechsolutions@gmail.com>
2026-08-06 20:03:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         onOpenLatency={() => setIsLatencyOpen(true)}
       />
 
-      <HeroSection />
+      <HeroSection onOpenPricing={() => setIsPricingOpen(true)} />
 
       <UniversalMarketplace onSelectTool={(tool) => setSelectedTool(tool)} />
 
